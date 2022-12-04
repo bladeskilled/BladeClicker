7eaa8f3831d16c603ff359382c3de8b35ffa0140
BladeSkilled <109127903+bladeskilled@users.noreply.github.com>
2022-12-03 16:19:57 -0800

    Add files via upload

---
+++ BladeClicker/BladeClicker/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace BladeClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Enable = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.CPSText = new System.Windows.Forms.Label();
+            this.Click = new System.Windows.Forms.Timer(this.components);
+            this.Random = new System.Windows.Forms.Timer(this.components);
+            this.Bind = new System.Windows.Forms.Button();
+            this.BindTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Enable
+            // 
+            this.Enable.AutoSize = true;
+            this.Enable.Font = new System.Drawing.Font("Aspekta 500", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Enable.Location = new System.Drawing.Point(6, 5);
+            this.Enable.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Enable.Name = "Enable";
+            this.Enable.Size = new System.Drawing.Size(106, 34);
+            this.Enable.TabIndex = 0;
+            this.Enable.Text = "Enable";
+            this.Enable.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(-1, 40);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(383, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // CPSText
+            // 
+            this.CPSText.AutoSize = true;
+            this.CPSText.Font = new System.Drawing.Font("Aspekta 500", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CPSText.Location = new System.Drawing.Point(0, 85);
+            this.CPSText.Name = "CPSText";
+            this.CPSText.Size = new System.Drawing.Size(99, 34);
+            this.CPSText.TabIndex = 2;
+            this.CPSText.Text = "10 CPS";
+            // 
+            // Click
+            // 
+            this.Click.Enabled = true;
+            this.Click.Tick += new System.EventHandler(this.Click_Tick);
+            // 
+            // Random
+            // 
+            this.Random.Enabled = true;
+            this.Random.Tick += new System.EventHandler(this.Random_Tick);
+            // 
+            // Bind
+            // 
+            this.Bind.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.Bind.Font = new System.Drawing.Font("Aspekta 500", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bind.Location = new System.Drawing.Point(105, 82);
+            this.Bind.Name = "Bind";
+            this.Bind.Size = new System.Drawing.Size(277, 40);
+            this.Bind.TabIndex = 3;
+            this.Bind.Text = "Bind = none";
+            this.Bind.UseVisualStyleBackColor = false;
+            this.Bind.Click += new System.EventHandler(this.Bind_Click);
+            this.Bind.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Bind_KeyUp);
+            // 
+            // BindTimer
+            // 
+            this.BindTimer.Enabled = true;
+            this.BindTimer.Interval = 150;
+            this.BindTimer.Tick += new System.EventHandler(this.BindTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(382, 131);
+            this.Controls.Add(this.Bind);
+            this.Controls.Add(this.CPSText);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Enable);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BladeClicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox Enable;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label CPSText;
+        private System.Windows.Forms.Timer Click;
+        private System.Windows.Forms.Timer Random;
+        private System.Windows.Forms.Button Bind;
+        private System.Windows.Forms.Timer BindTimer;
+    }
+}
+
---
+++ BladeClicker/BladeClicker/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BladeClicker
+{
+    public partial class Form1 : Form
+    {
+        #region dlls
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("User32.Dll", EntryPoint = "PostMessageA")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("User32.dll")]
+        public static extern short GetAsyncKeyState(Keys vKey);
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            CPSText.Text = $"{trackBar1.Value} CPS";
+        }
+
+        private void Click_Tick(object sender, EventArgs e)
+        {
+            Click.Interval = 1000 / trackBar1.Value;
+
+            Process[] processes = Process.GetProcessesByName("javaw");
+            foreach (Process process in processes)
+            {
+                if (GetForegroundWindow() == FindWindow(null, process.MainWindowTitle))
+                {
+                    if (Enable.Checked)
+                    {
+                        if (MouseButtons == MouseButtons.Left)
+                        {
+                            SendMessage(GetForegroundWindow(), 0x201, 0, 0);
+                            Task.Delay(20).Wait();
+                            SendMessage(GetForegroundWindow(), 0x202, 0, 0);
+                        }
+                    }
+                }
+            }
+        }
+
+        int RandomisedCPS = 10;
+        private void Random_Tick(object sender, EventArgs e)
+        {
+            RandomisedCPS = new Random().Next(trackBar1.Value - 3, trackBar1.Value + 3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bind.FlatStyle = FlatStyle.Flat;
+            Bind.FlatAppearance.BorderSize = 0;
+        }
+
+        private void Bind_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Bind.Text == "Press a key to bind")
+            {
+                if (GetAsyncKeyState(Keys.Escape) < 0)
+                {
+                    Bind.Text = "Bind = none";
+                }
+                else
+                {
+                    Bind.Text = e.KeyCode.ToString();
+                }
+            }
+        }
+
+        private void Bind_Click(object sender, EventArgs e)
+        {
+            Bind.Text = "Press a key to bind";
+        }
+
+        public static KeysConverter Key = new KeysConverter();
+        private void BindTimer_Tick(object sender, EventArgs e)
+        {
+            if (Bind.Text != "Bind = none")
+            {
+                if (Bind.Text != "Press a key to bind")
+                {
+                    Keys key = (Keys)Key.ConvertFromString(Bind.Text);
+
+                    if (GetAsyncKeyState(key) < 0)
+                    {
+                        Enable.Checked = !Enable.Checked;
+                    }
+                }
+            }
+        }
+    }
+}
